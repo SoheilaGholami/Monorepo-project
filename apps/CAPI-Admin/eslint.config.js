5b6de4b53318fb99d9c2d26e47f2a28c14197210
soheila gholami <s.gholami1995@gmail.com>
2024-11-26 18:41:52 +0330

    [CAPI-A] fix: remove base config

--- apps/CAPI-Admin/eslint.config.js
+++ apps/CAPI-Admin/eslint.config.js
@@ -1,8 +1,7 @@
 const { FlatCompat } = require("@eslint/eslintrc");
 const js = require("@eslint/js");
 const { fixupConfigRules } = require("@eslint/compat");
-const nx = require("@nx/eslint-plugin");
-const baseConfig = require("../../eslint.config.js");
+// const nx = require("@nx/eslint-plugin");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -14,8 +13,7 @@ module.exports = [
 
   ...fixupConfigRules(compat.extends("next/core-web-vitals")),
 
-  ...baseConfig,
-  ...nx.configs["flat/react-typescript"],
+  // ...nx.configs["flat/react-typescript"],
   {
     ignores: [".next/**/*"],
   },
